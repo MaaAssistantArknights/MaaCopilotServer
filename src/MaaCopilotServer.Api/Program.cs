dcbaa7741ad2825cc75bbd9899cfc901da0c3f50
Liam Sho <conan_creeper@outlook.com>
2022-06-07 08:34:30 +0800

    feat.添加 ElasticSearch 支持

--- src/MaaCopilotServer.Api/Program.cs
+++ src/MaaCopilotServer.Api/Program.cs
@@ -2,17 +2,35 @@
 // MaaCopilotServer belongs to the MAA organization.
 // Licensed under the AGPL-3.0 license.
 
+using Elastic.Apm.AspNetCore;
+using Elastic.Apm.EntityFrameworkCore;
 using MaaCopilotServer.Api;
 using MaaCopilotServer.Api.Helper;
 using MaaCopilotServer.Application;
 using MaaCopilotServer.Infrastructure;
 using Serilog;
+using Serilog.Sinks.Elasticsearch;
 
 var configuration = ConfigurationHelper.BuildConfiguration();
 
-Log.Logger = new LoggerConfiguration()
-    .ReadFrom.Configuration(configuration)
-    .CreateLogger();
+var loggerConfiguration = new LoggerConfiguration()
+    .ReadFrom.Configuration(configuration);
+
+if (configuration.GetValue<bool>("ElasticLogSink:Enabled"))
+{
+    var elasticUris = configuration.GetValue<string>("ElasticLogSink:Uris").Split(";").Select(x => new Uri(x)).ToArray();
+    var elasticPeriod = configuration.GetValue<int>("ElasticLogSink:Period");
+    var elasticApiId = configuration.GetValue<string>("ElasticLogSink:ApiId");
+    var elasticApiKey = configuration.GetValue<string>("ElasticLogSink:ApiKey");
+    loggerConfiguration.WriteTo.Elasticsearch(new ElasticsearchSinkOptions(elasticUris)
+    {
+        Period = TimeSpan.FromSeconds(elasticPeriod),
+        AutoRegisterTemplate = true,
+        IndexFormat = "maa-copilot-{0:yyyy.MM.dd}",
+        ModifyConnectionSettings = c =>
+            c.ApiKeyAuthentication(elasticApiId, elasticApiKey)
+    });
+}
 
 var builder = WebApplication.CreateBuilder();
 
@@ -26,6 +44,11 @@ builder.Services.AddApiServices(configuration);
 var app = builder.Build();
 
 DatabaseHelper.DatabaseInitialize(app.Services);
+
+if (configuration.GetValue<bool>("ElasticApm:Enabled"))
+{
+    app.UseElasticApm(configuration, new EfCoreDiagnosticsSubscriber());
+}
 
 app.UseAuthentication();
 app.MapControllers();
--- src/MaaCopilotServer.Api/Services/CurrentUserService.cs
+++ src/MaaCopilotServer.Api/Services/CurrentUserService.cs
@@ -10,10 +10,14 @@ namespace MaaCopilotServer.Api.Services;
 public class CurrentUserService : ICurrentUserService
 {
     private readonly IHttpContextAccessor _httpContextAccessor;
+    private readonly IConfiguration _configuration;
 
-    public CurrentUserService(IHttpContextAccessor httpContextAccessor)
+    public CurrentUserService(
+        IHttpContextAccessor httpContextAccessor,
+        IConfiguration configuration)
     {
         _httpContextAccessor = httpContextAccessor;
+        _configuration = configuration;
     }
 
     public Guid? GetUserIdentity()
@@ -30,6 +34,16 @@ public class CurrentUserService : ICurrentUserService
 
     public string GetTrackingId()
     {
+        if (_configuration.GetValue<bool>("ElasticApm:Enabled") is false)
+        {
+            return _httpContextAccessor.HttpContext?.TraceIdentifier ?? string.Empty;
+        }
+
+        var t = Elastic.Apm.Agent.Tracer.CurrentTransaction;
+        if (t is not null)
+        {
+            return t.TraceId;
+        }
         return _httpContextAccessor.HttpContext?.TraceIdentifier ?? string.Empty;
     }
 }
