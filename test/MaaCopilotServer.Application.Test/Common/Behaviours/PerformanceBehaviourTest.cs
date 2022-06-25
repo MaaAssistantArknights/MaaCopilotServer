e9950d6c93ba0d7c05e4d45defb1550e390e33b2
Helloworld <1326521+LYZhelloworld@users.noreply.github.com>
2022-06-25 15:25:04 +0800

    Add test for PerformanceBehaviour

--- test/MaaCopilotServer.Application.Test/Common/Behaviours/PerformanceBehaviourTest.cs
+++ test/MaaCopilotServer.Application.Test/Common/Behaviours/PerformanceBehaviourTest.cs
@@ -7,6 +7,7 @@ using MaaCopilotServer.Application.Common.Helpers;
 using MaaCopilotServer.Application.Common.Interfaces;
 using MaaCopilotServer.Application.Common.Models;
 using MediatR;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 
 namespace MaaCopilotServer.Application.Test.Common.Behaviours;
@@ -34,25 +35,64 @@ public class PerformanceBehaviourTest
     public void Initialize()
     {
         _currentUserService = Substitute.For<ICurrentUserService>();
-        _logger = Substitute.For<ILogger<IRequest<MaaApiResponse>>>();
+        _logger = new TestLogger();
     }
 
     /// <summary>
     ///     Tests
     ///     <see
-    ///         cref="PerformanceBehaviour{TRequest, TResponse}.Handle(TRequest, CancellationToken, MediatR.RequestHandlerDelegate{TResponse})" />
+    ///         cref="PerformanceBehaviour{TRequest, TResponse}.Handle(TRequest, CancellationToken, RequestHandlerDelegate{TResponse})" />
     ///     .
     /// </summary>
     /// <returns>N/A</returns>
-    [TestMethod]
-    public async Task TestHandle()
+    [DataTestMethod]
+    [DataRow(StatusCodes.Status200OK, LogLevel.Information)]
+    [DataRow(StatusCodes.Status400BadRequest, LogLevel.Warning)]
+    [DataRow(StatusCodes.Status401Unauthorized, LogLevel.Warning)]
+    [DataRow(StatusCodes.Status403Forbidden, LogLevel.Warning)]
+    [DataRow(StatusCodes.Status404NotFound, LogLevel.Warning)]
+    [DataRow(StatusCodes.Status500InternalServerError, LogLevel.Error)]
+    public async Task TestHandle(int statusCode, LogLevel expectedLogLevel)
     {
         var testUserId = new Guid();
         _currentUserService.GetUserIdentity().Returns(testUserId);
         var behaviour =
             new PerformanceBehaviour<IRequest<MaaApiResponse>, MaaApiResponse>(_logger, _currentUserService);
-        var action = async () =>
-            await behaviour.Handle(null, new CancellationToken(), () => Task.FromResult(MaaApiResponseHelper.Ok()));
-        await action.Should().NotThrowAsync();
+        await behaviour.Handle(null, new CancellationToken(), () => Task.FromResult(new MaaApiResponse()
+        {
+            StatusCode = statusCode,
+        }));
+        var testLogger = (TestLogger)_logger;
+        testLogger.Called.Should()
+                         .HaveCount(1)
+                         .And
+                         .Contain(expectedLogLevel);
+    }
+
+    /// <summary>
+    /// The test class for logger.
+    /// </summary>
+    private class TestLogger : ILogger<IRequest<MaaApiResponse>>
+    {
+        /// <summary>
+        /// Called arguments of log level.
+        /// </summary>
+        public readonly List<LogLevel> Called = new();
+
+        /// <inheritdoc/>
+        public IDisposable BeginScope<TState>(TState state) => default;
+
+        /// <inheritdoc/>
+        public bool IsEnabled(LogLevel logLevel) => true;
+
+        /// <inheritdoc/>
+        public void Log<TState>(LogLevel logLevel,
+                                EventId eventId,
+                                TState state,
+                                Exception? exception,
+                                Func<TState, Exception?, string> formatter)
+        {
+            Called.Add(logLevel);
+        }
     }
 }
