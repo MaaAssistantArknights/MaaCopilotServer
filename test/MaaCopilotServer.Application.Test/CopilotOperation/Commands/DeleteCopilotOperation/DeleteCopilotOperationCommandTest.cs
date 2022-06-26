9eeb6ff6614e106a4e6636f5a2ef426ff03ba4c2
Helloworld <1326521+LYZhelloworld@users.noreply.github.com>
2022-06-26 21:20:59 +0800

    Add tests for DeleteCopilotOperationCommandHandler

--- test/MaaCopilotServer.Application.Test/CopilotOperation/Commands/DeleteCopilotOperation/DeleteCopilotOperationCommandTest.cs
+++ test/MaaCopilotServer.Application.Test/CopilotOperation/Commands/DeleteCopilotOperation/DeleteCopilotOperationCommandTest.cs
@@ -4,6 +4,7 @@
 
 using MaaCopilotServer.Application.Common.Interfaces;
 using MaaCopilotServer.Application.CopilotOperation.Commands.DeleteCopilotOperation;
+using MaaCopilotServer.Domain.Enums;
 using MaaCopilotServer.Infrastructure.Services;
 using MaaCopilotServer.Test.TestHelpers;
 using Microsoft.AspNetCore.Http;
@@ -38,16 +39,17 @@ public class DeleteCopilotOperationCommandTest
     private readonly IMaaCopilotDbContext _dbContext = new TestDbContext();
 
     /// <summary>
-    /// Tests <see cref="DeleteCopilotOperationCommandHandler.Handle(DeleteCopilotOperationCommand, CancellationToken)"/>.
+    /// Tests <see cref="DeleteCopilotOperationCommandHandler.Handle(DeleteCopilotOperationCommand, CancellationToken)"/>
+    /// with the same user.
     /// </summary>
     [TestMethod]
-    public void TestHandle()
+    public void TestHandle_SameUser()
     {
         var user = new Domain.Entities.CopilotUser(
             string.Empty,
             string.Empty,
             string.Empty,
-            Domain.Enums.UserRole.User,
+            UserRole.User,
             Guid.Empty);
         _dbContext.CopilotUsers.Add(user);
         _dbContext.SaveChangesAsync(new CancellationToken()).Wait();
@@ -75,6 +77,71 @@ public class DeleteCopilotOperationCommandTest
 
         result.StatusCode.Should().Be(StatusCodes.Status200OK);
         _dbContext.CopilotOperations.Any().Should().BeFalse();
+    }
+
+    /// <summary>
+    /// Tests <see cref="DeleteCopilotOperationCommandHandler.Handle(DeleteCopilotOperationCommand, CancellationToken)"/>
+    /// with accessing from an account to content created by other people.
+    /// </summary>
+    [DataTestMethod]
+    [DataRow(UserRole.User, UserRole.User, false)]
+    [DataRow(UserRole.Admin, UserRole.User, true)]
+    [DataRow(UserRole.Admin, UserRole.Uploader, true)]
+    [DataRow(UserRole.Admin, UserRole.Admin, false)]
+    [DataRow(UserRole.Admin, UserRole.SuperAdmin, false)]
+    [DataRow(UserRole.SuperAdmin, UserRole.User, true)]
+    [DataRow(UserRole.SuperAdmin, UserRole.Uploader, true)]
+    [DataRow(UserRole.SuperAdmin, UserRole.Admin, true)]
+    [DataRow(UserRole.SuperAdmin, UserRole.SuperAdmin, true)]
+    public void TestHandle_DifferentUsers(UserRole requesterRole, UserRole authorRole, bool expectedToSucceed)
+    {
+        var user = new Domain.Entities.CopilotUser(
+            string.Empty,
+            string.Empty,
+            string.Empty,
+            requesterRole,
+            Guid.Empty);
+        var author = new Domain.Entities.CopilotUser(
+            string.Empty,
+            string.Empty,
+            string.Empty,
+            authorRole,
+            Guid.Empty);
+        _dbContext.CopilotUsers.Add(user);
+        _dbContext.CopilotUsers.Add(author);
+        _dbContext.SaveChangesAsync(new CancellationToken()).Wait();
+        var entity = new Domain.Entities.CopilotOperation(
+            1,
+            string.Empty,
+            string.Empty,
+            string.Empty,
+            string.Empty,
+            string.Empty,
+            author,
+            Guid.Empty,
+            new List<string>());
+        _dbContext.CopilotOperations.Add(entity);
+        _dbContext.SaveChangesAsync(new CancellationToken()).Wait();
+        var currentUserService = new Mock<ICurrentUserService>();
+        currentUserService.Setup(x => x.GetUser().Result).Returns(user);
+
+        var handler = new DeleteCopilotOperationCommandHandler(
+            _dbContext, _copilotIdService, currentUserService.Object, _apiErrorMessage);
+        var result = handler.Handle(new DeleteCopilotOperationCommand()
+        {
+            Id = _copilotIdService.EncodeId(entity.Id)
+        }, new CancellationToken()).GetAwaiter().GetResult();
+
+        if (expectedToSucceed)
+        {
+            result.StatusCode.Should().Be(StatusCodes.Status200OK);
+            _dbContext.CopilotOperations.Any().Should().BeFalse();
+        }
+        else
+        {
+            result.StatusCode.Should().Be(StatusCodes.Status403Forbidden);
+            _dbContext.CopilotOperations.Any().Should().BeTrue();
+        }
     }
 
     /// <summary>
