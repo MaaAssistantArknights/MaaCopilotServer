47adca72306433281e057df2d0a2583082f7cc45
Helloworld <1326521+LYZhelloworld@users.noreply.github.com>
2022-07-04 12:52:06 +0800

    Test system controller

--- test/MaaCopilotServer.Api.Test/TestHelpers/ControllerTestHelper.cs
+++ test/MaaCopilotServer.Api.Test/TestHelpers/ControllerTestHelper.cs
@@ -24,17 +24,33 @@ public static class ControllerTestHelper
     public static void Test<TController>(Func<TController, Task<ActionResult>> testMethod)
         where TController : MaaControllerBase
     {
+        var controllerType = typeof(TController);
+
+        // Get controller constructor which takes 1 argument of IMediator type.
+        var ctor = controllerType.GetConstructor(new Type[] { typeof(IMediator) });
+
+        // Test controller.
+        Test(mediator => (TController)ctor!.Invoke(new object[] { mediator }), testMethod);
+    }
+
+    /// <summary>
+    ///     Tests an endpoint of the controller.
+    /// </summary>
+    /// <typeparam name="TController">
+    ///     The type of the controller, which should be a derived class of <see cref="MaaControllerBase"/>.
+    /// </typeparam>
+    /// <param name="controllerBuilder">The builder function of the controller.</param>
+    /// <param name="testMethod">The function to call a controller method.</param>
+    public static void Test<TController>(Func<IMediator, TController> controllerBuilder, Func<TController, Task<ActionResult>> testMethod)
+        where TController : MaaControllerBase
+    {
         var testResponse = new object();
         var testResponseData = MaaApiResponseHelper.Ok(testResponse);
         var mediator = new Mock<IMediator>();
         mediator.Setup(x => x.Send(It.IsAny<It.IsAnyType>(), It.IsAny<CancellationToken>()).Result)
                 .Returns(testResponseData);
 
-        var controllerType = typeof(TController);
-
-        // Get controller constructor which takes 1 argument of IMediator type.
-        var ctor = controllerType.GetConstructor(new Type[] { typeof(IMediator) });
-        var controller = (TController)ctor!.Invoke(new object[] { mediator.Object });
+        var controller = controllerBuilder(mediator.Object);
 
         var response = testMethod.Invoke(controller).GetAwaiter().GetResult();
         response.Should().NotBeNull();
