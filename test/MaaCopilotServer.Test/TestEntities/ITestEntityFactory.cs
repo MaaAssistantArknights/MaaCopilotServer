4974b32ab95aae65ef86f18b2c7cebec594b20be
Helloworld <1326521+LYZhelloworld@users.noreply.github.com>
2022-07-14 20:46:32 +0800

    Limit TEntity range

--- test/MaaCopilotServer.Test/TestEntities/ITestEntityFactory.cs
+++ test/MaaCopilotServer.Test/TestEntities/ITestEntityFactory.cs
@@ -2,6 +2,8 @@
 // MaaCopilotServer belongs to the MAA organization.
 // Licensed under the AGPL-3.0 license.
 
+using MaaCopilotServer.Domain.Common;
+
 namespace MaaCopilotServer.Test.TestEntities;
 
 /// <summary>
@@ -9,6 +11,7 @@ namespace MaaCopilotServer.Test.TestEntities;
 /// </summary>
 /// <typeparam name="TEntity">The type of the entity.</typeparam>
 public interface ITestEntityFactory<TEntity>
+    where TEntity : BaseEntity
 {
     /// <summary>
     /// Builds entity.
