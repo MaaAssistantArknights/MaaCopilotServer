89cc55336b8d413ffdf5b11c4bda3749e9aa58ef
Helloworld <1326521+LYZhelloworld@users.noreply.github.com>
2022-08-06 17:18:34 +0800

    更新 controller 文档

--- src/MaaCopilotServer.Api/Constants/SystemStatus.cs
+++ src/MaaCopilotServer.Api/Constants/SystemStatus.cs
@@ -6,12 +6,24 @@ using System.Diagnostics.CodeAnalysis;
 
 namespace MaaCopilotServer.Api.Constants;
 
+/// <summary>
+/// The system statuses.
+/// </summary>
 [ExcludeFromCodeCoverage]
 public static class SystemStatus
 {
+    /// <summary>
+    /// A value that indicates whether the system has been fully initialised.
+    /// </summary>
     public static bool IsOk => DatabaseInitialized && ArknightsDataInitialized;
 
+    /// <summary>
+    /// A value that indicates whether the database has been initialised. 
+    /// </summary>
     public static bool DatabaseInitialized { get; set; } = false;
 
+    /// <summary>
+    /// A value that indicates whether the Arknights data has been initialised.
+    /// </summary>
     public static bool ArknightsDataInitialized { get; set; } = false;
 }
