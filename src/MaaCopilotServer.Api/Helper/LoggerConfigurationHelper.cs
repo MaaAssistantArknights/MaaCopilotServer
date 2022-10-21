b4d3f751e63d1299ba9e2701beb8c8a2e365c037
Liam Sho <conan_creeper@outlook.com>
2022-10-21 10:43:43 +0800

    chore: 移除 ELK Stack 集成

--- src/MaaCopilotServer.Api/Helper/LoggerConfigurationHelper.cs
+++ src/MaaCopilotServer.Api/Helper/LoggerConfigurationHelper.cs
@@ -4,12 +4,7 @@
 
 using System.Diagnostics.CodeAnalysis;
 using Destructurama;
-using Elastic.Apm.SerilogEnricher;
-using Elastic.CommonSchema.Serilog;
-using MaaCopilotServer.Application.Common.Extensions;
-using MaaCopilotServer.Domain.Options;
 using Serilog;
-using Serilog.Sinks.Elasticsearch;
 
 namespace MaaCopilotServer.Api.Helper;
 
@@ -26,44 +21,9 @@ public static class LoggerConfigurationHelper
     /// <returns>The <see cref="LoggerConfiguration" /> instance.</returns>
     public static LoggerConfiguration GetLoggerConfiguration(this IConfiguration configuration)
     {
-        var switchesOption = configuration.GetOption<SwitchesOption>();
         var loggerConfiguration = new LoggerConfiguration()
             .ReadFrom.Configuration(configuration)
             .Destructure.UsingAttributes();
-
-        if (switchesOption.ElasticSearch is false)
-        {
-            return loggerConfiguration;
-        }
-
-        // Elastic search options.
-        var elasticOptions = configuration.GetOption<ElasticLogSinkOption>();
-        var elasticUris = elasticOptions.Uris.Split(";").Select(x => new Uri(x)).ToArray();
-        loggerConfiguration.WriteTo.Elasticsearch(new ElasticsearchSinkOptions(elasticUris)
-        {
-            Period = TimeSpan.FromSeconds(elasticOptions.Period),
-            AutoRegisterTemplate = true,
-            IndexFormat = $"{elasticOptions.ApplicationName}-{DateTimeOffset.UtcNow.AddHours(8):yyyy.MM}",
-            CustomFormatter = new EcsTextFormatter(),
-            TypeName = null,
-            ModifyConnectionSettings = c =>
-            {
-                switch (elasticOptions.Authentication.Method)
-                {
-                    case "Basic":
-                        c.BasicAuthentication(elasticOptions.Authentication.Secret.Id,
-                            elasticOptions.Authentication.Secret.Key);
-                        break;
-                    case "ApiKey":
-                        c.ApiKeyAuthentication(elasticOptions.Authentication.Secret.Id,
-                            elasticOptions.Authentication.Secret.Key);
-                        break;
-                }
-
-                return c;
-            }
-        });
-        loggerConfiguration.Enrich.WithElasticApmCorrelationInfo();
 
         return loggerConfiguration;
     }
